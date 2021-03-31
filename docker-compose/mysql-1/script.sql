4da8aeff61a5cf8eafc1149432d3a81e5d6915b4
omkar-21 <53621647+omkar-21@users.noreply.github.com>
2021-03-31 11:12:17 +0530

    Add files via upload

---
+++ docker-compose/mysql-1/script.sql
@@ -0,0 +1,9 @@
+create user 'omkar'@'localhost' IDENTIFIED BY 'pucsd';
+CREATE DATABASE pucsdStudents;
+GRANT ALL PRIVILEGES ON pucsdStudents . * TO 'omkar'@'localhost';
+USE pucsdStudents;
+
+CREATE TABLE Students (name VARCHAR(20),Roll_no VARCHAR(20),address text,mobile_number BIGINT,pan_number varchar(20));
+
+INSERT INTO Students VALUES ('Omkar Shirke','18148','aundh road','781237318','asd123889');
+1
